ba6209c452e288783e35faa82815e934dbfb7bac
Pavel Belokon <pbelokon@myseneca.ca>
2024-02-07 20:58:48 -0500

    added link to 2d virsion

--- src/pages/plain/components/Header.jsx
+++ src/pages/plain/components/Header.jsx
@@ -2,8 +2,8 @@ export default function Header() {
   return (
     <header>
       <h1>Pavel Belokon </h1>
-      <a href="#" className="to3D">
-        3D VERSION
+      <a href="https://portfolio-chi-ten-14.vercel.app/" className="to3D">
+        2D VERSION
       </a>
     </header>
   );
