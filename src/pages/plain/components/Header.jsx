cdb0a10aa486f8f161fd7858179a9eda85f81976
Pavel Belokon <pbelokon@myseneca.ca>
2024-02-07 21:03:02 -0500

    fixed button for 3d

--- src/pages/plain/components/Header.jsx
+++ src/pages/plain/components/Header.jsx
@@ -2,8 +2,8 @@ export default function Header() {
   return (
     <header>
       <h1>Pavel Belokon </h1>
-      <a href="https://portfolio-chi-ten-14.vercel.app/" className="to3D">
-        2D VERSION
+      <a href="https://www.pbelokon.com/" className="to3D">
+        3D VERSION
       </a>
     </header>
   );
